70123f04ad5eca375b0b5c341e2a5d8ae019155d
ershao.shi <1836340251@qq.com>
2020-12-19 10:04:15 +0800

    te

---
+++ NewDemo/Controllers/UsersController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NewDemo.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NewDemo.Controllers
+{
+    [Route("[controller]/[action]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        public UsersController() { }
+        [HttpGet]
+        public UserInfo GetUser()
+        {
+            return new UserInfo()
+            {
+                UserAge = 20,
+                UserName = "ershao",
+                Gender = "男"
+            };
+        }
+    }
+}
---
+++ NewDemo/Model/UserInfo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NewDemo.Model
+{
+    public class UserInfo
+    {
+        public string UserName { get; set; }
+        public int UserAge { get; set; }
+        public string Gender { get; set; }
+    }
+}
